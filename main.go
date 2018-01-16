da2da240d29b59dd2c7d1a146067adcc0a792686
Openset <openset.wang@gmail.com>
2018-01-16 11:03:37 +0800

    Add: ord

--- main.go
+++ main.go
@@ -5,4 +5,6 @@ import "github.com/openset/php2go/php"
 func main() {
 	php.Echo("Hello ", "world!\n")
 	php.Print("This is a string.\n")
+	println(php.Chr(65))
+	println(php.Ord('A'))
 }
---
+++ php/Ord.go
@@ -0,0 +1,6 @@
+package php
+
+//返回字符的 ASCII 码值
+func Ord(s byte) byte {
+	return s
+}
