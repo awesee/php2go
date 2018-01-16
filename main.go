e6bf7fdd93cff8a2825aa6c5dee0ba67a9119d43
Openset <openset.wang@gmail.com>
2018-01-16 10:53:11 +0800

    Add: chr

--- main.go
+++ main.go
@@ -4,4 +4,5 @@ import "github.com/openset/php2go/php"
 
 func main() {
 	php.Echo("Hello ", "world!\n")
+	php.Print("This is a string.\n")
 }
---
+++ php/chr.go
@@ -0,0 +1,6 @@
+package php
+
+//返回指定的字符
+func Chr(int int) string {
+	return string(int)
+}
