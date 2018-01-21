b6f872024fe3f3b31060fbf900e8be9b2cd62ca1
Openset <openset.wang@gmail.com>
2018-01-21 16:55:15 +0800

    Add: array_chunk

---
+++ php/array_chunk.go
@@ -0,0 +1,21 @@
+package php
+
+import (
+	"fmt"
+	"math"
+)
+
+type ArraySlice []interface{}
+
+func ArrayChunk(input ArraySlice, size int) ArraySlice {
+	length := len(input)
+	count := int(math.Ceil(float64(length) / float64(size)))
+	ret := make(ArraySlice, count)
+	for i := 0; i < count-1; i++ {
+		ret[i] = input[i*size : (i+1)*size]
+		fmt.Println(ret)
+	}
+	ret[count-1] = input[size*(count-1):]
+
+	return ret
+}
