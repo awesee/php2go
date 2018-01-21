1f0f8e565b3cc507bf6fb4dd2d14f1088852e6da
Openset <openset.wang@gmail.com>
2018-01-21 17:04:08 +0800

    Add: base64_decode

---
+++ php/base64.go
@@ -0,0 +1,18 @@
+package php
+
+import "encoding/base64"
+
+//Encodes data with MIME base64
+func Base64Encode(s string) string {
+	return base64.StdEncoding.EncodeToString([]byte(s))
+}
+
+//Decodes data encoded with MIME base64
+func Base64Decode(s string) (string, error) {
+	bt, err := base64.StdEncoding.DecodeString(s)
+	if err != nil {
+		return "", err
+	}
+
+	return string(bt), nil
+}
