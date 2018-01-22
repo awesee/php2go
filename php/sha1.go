0ec27821d4f4a190f7ea49c2f0ebc055a98ed16e
Openset <openset.wang@gmail.com>
2018-01-22 13:20:06 +0800

    Add: sha1_file

--- php/sha1.go
+++ php/sha1.go
@@ -3,10 +3,22 @@ package php
 import (
 	"crypto/sha1"
 	"encoding/hex"
+	"io/ioutil"
 )
 
 //Calculate the sha1 hash of a string
 func Sha1(s string) string {
 	byte := sha1.Sum([]byte(s))
+	return hex.EncodeToString(byte[:])
+}
+
+//Calculates the md5 hash of a given file
+func Sha1File(filename string) string {
+	data, err := ioutil.ReadFile(filename)
+	if err != nil {
+		return ""
+	}
+	byte := sha1.Sum(data)
+
 	return hex.EncodeToString(byte[:])
 }
