ade93ddcc7ec77c31c18f06fca3fb3917319dd7a
Openset <openset.wang@gmail.com>
2018-01-23 15:54:11 +0800

    Add: basename

---
+++ php/basename.go
@@ -0,0 +1,17 @@
+package php
+
+import (
+	"strings"
+)
+
+//Returns trailing name component of path
+func Basename(s string, suffix ...string) string {
+	slash := strings.LastIndex(s, "/")
+	s = s[slash+1:]
+	if len(suffix) > 0 {
+		suf := strings.LastIndex(s, suffix[0])
+		s = s[:suf]
+	}
+
+	return s
+}
