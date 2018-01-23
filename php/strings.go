f44ca17695b29d42d8ee93e09ffe6c3baadc715f
Openset <openset.wang@gmail.com>
2018-01-23 16:40:07 +0800

    Add: strings

--- php/strings.go
+++ php/strings.go
@@ -1,9 +1,22 @@
 package php
 
-import "strings"
+import (
+	"strings"
+)
 
 //Find the position of the first occurrence of a substring in a string
 func Strpos(s, substr string) int {
 
 	return strings.Index(s, substr)
+}
+
+//Find the last occurrence of a character in a string
+func Strrchr(s, substr string) string {
+
+	i := strings.LastIndex(s, substr)
+	if i < 0 {
+		return ""
+	}
+
+	return s[i:]
 }
