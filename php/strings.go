b143ee08bdf06c94bd69b0857231879757183f88
Openset <openset.wang@gmail.com>
2018-01-23 16:41:35 +0800

    Add: hex2bin

--- php/strings.go
+++ php/strings.go
@@ -1,13 +1,26 @@
 package php
 
 import (
+	"strconv"
 	"strings"
 )
+
+func Bin2hex(b string) string {
+
+	base, _ := strconv.ParseInt(b, 2, 64)
+	return strconv.FormatInt(base, 16)
+}
 
 //Find the position of the first occurrence of a substring in a string
 func Strpos(s, substr string) int {
 
 	return strings.Index(s, substr)
+}
+
+// Find the position of the last occurrence of a substring in a string
+func Strrpos(s, substr string) int {
+
+	return strings.LastIndex(s, substr)
 }
 
 //Find the last occurrence of a character in a string
--- php/strrpos.go
+++
@@ -1,9 +0,0 @@
-package php
-
-import "strings"
-
-// Find the position of the last occurrence of a substring in a string
-func Strrpos(s, substr string) int {
-
-	return strings.LastIndex(s, substr)
-}
