2f17f1eb1ed218a47b5aeea265437a3e16fa77ae
Openset <openset.wang@gmail.com>
2018-01-23 18:12:37 +0800

    Add: addslashes

---
+++ php/addslashes.go
@@ -0,0 +1,19 @@
+package php
+
+//Quote string with slashes
+func Addslashes(s string) string {
+
+	l := len(s)
+	r := make([]byte, l, l*2)
+	for i := 0; i < l; i++ {
+
+		switch s[i] {
+		case '\'', '"', '\\':
+			r = append(r, '\\')
+		}
+
+		r = append(r, s[i])
+	}
+
+	return string(r)
+}
