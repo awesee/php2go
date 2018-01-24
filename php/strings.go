f16dfa6d0f27a0a551b79beac029bdff956709cb
Openset <openset.wang@gmail.com>
2018-01-24 12:30:13 +0800

    Add: strstr

--- php/strings.go
+++ php/strings.go
@@ -80,6 +80,12 @@ func Rtrim(s, cutset string) string {
 	return strings.TrimRight(s, cutset)
 }
 
+//Find the first occurrence of a string
+func Strstr(s, substr string) int {
+
+	return strings.Index(s, substr)
+}
+
 //Find the position of the first occurrence of a substring in a string
 func Strpos(s, substr string) int {
 
