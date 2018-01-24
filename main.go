17cd117892aa086ed6e84f867370fd43c71ae922
Openset <openset.wang@gmail.com>
2018-01-24 12:56:44 +0800

    Update: main

--- main.go
+++ main.go
@@ -2,23 +2,37 @@ package main
 
 import (
 	"fmt"
-	"./php"
+	"github.com/openset/php2go/php"
 )
 
 func main() {
+
 	php.Echo("Hello ", "world!\n")
+
 	php.Print("This is a string.\n")
+
 	println(php.Chr(65))
+
 	println(php.Ord('A'))
+
 	fmt.Println(php.Array(1, 'a', "ABC"))
-	fmt.Println(php.Strrev("Hello world!"))
-	fmt.Println(php.Strrev("你好，世界"))
+
+	println(php.Strrev("Hello world!"))
+
+	println(php.Strrev("你好，世界"))
+
 	fmt.Println(php.ArrayReverse(php.Array(1, 'a', "ABC")))
+
 	println(php.SysGetTempDir())
+
 	println(php.IsNumeric("-123.45"))
-	fmt.Println(php.Basename("abc/cde.ext", "xt"))
-	fmt.Println(php.Addcslashes("abc/cde.ext", 'c'))
-	fmt.Println(php.Addslashes("abc/'\"c\\de.ext"))
+
+	println(php.Basename("abc/cde.ext", "xt"))
+
+	println(php.Addcslashes("abc/cde.ext", 'c'))
+
+	println(php.Addslashes("abc/'\"c\\de.ext"))
 
 	println(php.Htmlspecialchars("This is some <b>bold</b> text."))
+
 }
