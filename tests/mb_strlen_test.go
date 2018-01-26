ff267ca051e8cddbf2be50a79750435dbec6e2e2
Openset <openset.wang@gmail.com>
2018-01-26 09:58:41 +0800

    Add: tests

---
+++ tests/mb_strlen_test.go
@@ -0,0 +1,34 @@
+package php_test
+
+import (
+	. "github.com/openset/php2go/php"
+	"testing"
+)
+
+// +------------------------------------------------------------
+// | @desc      Changes the case of all keys in an array
+// | @param     arr  ArrayMap
+// | @return    ArrayMap
+// |
+// | @author    Openset <openset.wang@gmail.com>
+// | @link      https://github.com/openset
+// | @date      2018/01/24
+// +------------------------------------------------------------
+func TestMbStrlen(t *testing.T) {
+
+	s := "A"
+	if MbStrlen(s) != 1 {
+		t.Errorf("MbStrlen() should have returned 1 for string [%s]", s)
+	}
+
+	s = "abc"
+	if MbStrlen(s) != 3 {
+		t.Errorf("MbStrlen() should have returned 3 for string [%s]", s)
+	}
+
+	s = "你好，世界"
+	if MbStrlen(s) != 5 {
+		t.Errorf("MbStrlen() should have returned 5 for string [%s]", s)
+	}
+
+}
