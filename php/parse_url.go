a255f3c020ac8d2d2850f8720189ff8e09df8e2a
Openset <openset.wang@gmail.com>
2018-01-30 14:19:49 +0800

    Add: ParseStr

--- php/parse_url.go
+++
@@ -1,8 +0,0 @@
-package php
-
-import "net/url"
-
-func ParseUrl(s string) (*url.URL, error) {
-
-	return url.Parse(s)
-}
---
+++ php/url.go
@@ -0,0 +1,15 @@
+package php
+
+import "net/url"
+
+//Parse a URL and return its components
+func ParseUrl(rawurl string) (*url.URL, error) {
+
+	return url.Parse(rawurl)
+}
+
+//Parses the string into variables
+func ParseStr(query string) (url.Values, error) {
+
+	return url.ParseQuery(query)
+}
