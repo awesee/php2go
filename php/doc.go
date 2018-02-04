734cece7cd87bc07682117cde0f1c7b616130f2d
Openset <openset.wang@gmail.com>
2018-02-04 20:48:32 +0800

    Add: doc

---
+++ php/doc.go
@@ -0,0 +1,9 @@
+// Copyright 2018 Openset. All rights reserved.
+// Use of this source code is governed by a MIT
+// license that can be found in the LICENSE file.
+
+/*
+PHP built-in function library with Go language
+*/
+
+package php // import "github.com/openset/php2go/php"
