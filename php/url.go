8652e8509f512396223ad764bfdbbffd40df1a7e
Openset <openset.wang@gmail.com>
2018-02-04 17:25:03 +0800

    Update: comment

--- php/url.go
+++ php/url.go
@@ -5,37 +5,37 @@ import (
 	"strings"
 )
 
-//Parse a URL and return its components
-func ParseUrl(rawurl string) (*url.URL, error) {
+//ParseURL - Parse a URL and return its components
+func ParseURL(rawurl string) (*url.URL, error) {
 
 	return url.Parse(rawurl)
 }
 
-//Parses the string into variables
+//ParseStr - Parses the string into variables
 func ParseStr(query string) (url.Values, error) {
 
 	return url.ParseQuery(query)
 }
 
-//URL-encode according to RFC 3986
+//Rawurlencode - URL-encode according to RFC 3986
 func Rawurlencode(s string) string {
 
 	return url.PathEscape(s)
 }
 
-//Decodes URL-encoded string
+//Rawurldecode - Decodes URL-encoded string
 func Rawurldecode(s string) (string, error) {
 
 	return url.PathUnescape(s)
 }
 
-//URL-encodes string
+//Urlencode - URL-encodes string
 func Urlencode(s string) string {
 
 	return strings.Replace(url.PathEscape(s), "%20", "+", -1)
 }
 
-//Decodes URL-encoded string
+//Urldecode - Decodes URL-encoded string
 func Urldecode(s string) (string, error) {
 
 	return url.PathUnescape(strings.Replace(s, "+", "%20", -1))
