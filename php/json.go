62ebe58e197fb1894ca84b3c9b90fd04247f81f5
Openset <openset.wang@gmail.com>
2018-02-04 17:49:05 +0800

    Update: comment

--- php/json.go
+++ php/json.go
@@ -4,13 +4,13 @@ import (
 	"encoding/json"
 )
 
-//Returns the JSON representation of a value
+//JsonEncode - Returns the JSON representation of a value
 func JsonEncode(data []byte, v interface{}) error {
 
 	return json.Unmarshal(data, v)
 }
 
-//Decodes a JSON string
+//JsonDecode - Decodes a JSON string
 func JsonDecode(v interface{}) ([]byte, error) {
 
 	return json.Marshal(v)
