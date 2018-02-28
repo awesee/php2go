b42bd5f3bbc0473518b7a8c5a9d5bd5dec1b3714
Openset <openset.wang@gmail.com>
2018-02-28 17:39:40 +0800

    Update: array push comment

--- php/array_push.go
+++ php/array_push.go
@@ -1,6 +1,6 @@
 package php
 
-//ArrayPush - Push one or more elements onto the end of array
+// ArrayPush - Push one or more elements onto the end of array
 func ArrayPush(s *[]string, args ...string) {
 
 	*s = append(*s, args...)
