a4eaff7ac844bcc892ee3cf9f063ded6ccec810c
Openset <openset.wang@gmail.com>
2018-02-28 18:00:13 +0800

    Update: strings comment

--- php/strings.go
+++ php/strings.go
@@ -13,7 +13,7 @@ const (
 	StrPadLeft  = "STR_PAD_LEFT"
 )
 
-//Bin2hex - Convert binary data into hexadecimal representation
+// Bin2hex - Convert binary data into hexadecimal representation
 func Bin2hex(b string) string {
 
 	base, err := strconv.ParseInt(b, 2, 64)
@@ -24,7 +24,7 @@ func Bin2hex(b string) string {
 	return strconv.FormatInt(base, 16)
 }
 
-//Bindec - Binary to decimal
+// Bindec - Binary to decimal
 func Bindec(b string) int64 {
 
 	i, _ := strconv.ParseInt(b, 2, 64)
@@ -32,7 +32,7 @@ func Bindec(b string) int64 {
 	return i
 }
 
-//Hex2bin - Decodes a hexadecimally encoded binary string
+// Hex2bin - Decodes a hexadecimally encoded binary string
 func Hex2bin(x string) string {
 
 	base, err := strconv.ParseInt(x, 16, 64)
@@ -43,7 +43,7 @@ func Hex2bin(x string) string {
 	return strconv.FormatInt(base, 2)
 }
 
-//Chr - Return a specific character
+// Chr - Return a specific character
 func Chr(ascii int) string {
 
 	for ascii < 0 {
@@ -54,13 +54,13 @@ func Chr(ascii int) string {
 	return string(ascii)
 }
 
-//Ord - Return ASCII value of character
+// Ord - Return ASCII value of character
 func Ord(s byte) byte {
 
 	return s
 }
 
-//Explode - Split a string by string
+// Explode - Split a string by string
 func Explode(s, sep string) []string {
 
 	if s == "" {
@@ -70,7 +70,7 @@ func Explode(s, sep string) []string {
 	return strings.Split(s, sep)
 }
 
-//GetHtmlTranslationTable - Returns the translation table used by htmlspecialchars() and htmlentities()
+// GetHtmlTranslationTable - Returns the translation table used by htmlspecialchars() and htmlentities()
 func GetHtmlTranslationTable() map[string]string {
 
 	return map[string]string{
@@ -81,49 +81,49 @@ func GetHtmlTranslationTable() map[string]string {
 	}
 }
 
-//Htmlspecialchars - Convert special characters to HTML entities
+// Htmlspecialchars - Convert special characters to HTML entities
 func Htmlspecialchars(s string) string {
 
 	return html.EscapeString(s)
 }
 
-//HtmlspecialcharsDecode - Convert special HTML entities back to characters
+// HtmlspecialcharsDecode - Convert special HTML entities back to characters
 func HtmlspecialcharsDecode(s string) string {
 
 	return html.UnescapeString(s)
 }
 
-//Implode - Join array elements with a string
+// Implode - Join array elements with a string
 func Implode(a []string, sep string) string {
 
 	return strings.Join(a, sep)
 }
 
-//Join - Alias of implode()
+// Join - Alias of implode()
 func Join(a []string, sep string) string {
 
 	return Implode(a, sep)
 }
 
-//Trim - Strip whitespace (or other characters) from the beginning and end of a string
+// Trim - Strip whitespace (or other characters) from the beginning and end of a string
 func Trim(s, cutset string) string {
 
 	return strings.Trim(s, cutset)
 }
 
-//Ltrim - Strip whitespace (or other characters) from the beginning of a string
+// Ltrim - Strip whitespace (or other characters) from the beginning of a string
 func Ltrim(s, cutset string) string {
 
 	return strings.TrimLeft(s, cutset)
 }
 
-//Rtrim - Strip whitespace (or other characters) from the end of a string
+// Rtrim - Strip whitespace (or other characters) from the end of a string
 func Rtrim(s, cutset string) string {
 
 	return strings.TrimRight(s, cutset)
 }
 
-//Nl2br - Inserts HTML line breaks before all newlines in a string
+// Nl2br - Inserts HTML line breaks before all newlines in a string
 func Nl2br(s string) string {
 
 	runes := []rune(s)
@@ -139,7 +139,7 @@ func Nl2br(s string) string {
 	return string(html)
 }
 
-//StrPad - Pad a string to a certain length with another string
+// StrPad - Pad a string to a certain length with another string
 func StrPad(s string, length int, args ...string) string {
 
 	runes := []rune(s)
@@ -169,43 +169,43 @@ func StrPad(s string, length int, args ...string) string {
 	return out
 }
 
-//StrRepeat - Repeat a string
+// StrRepeat - Repeat a string
 func StrRepeat(s string, count int) string {
 
 	return strings.Repeat(s, count)
 }
 
-//StrReplace - Replace all occurrences of the search string with the replacement string
+// StrReplace - Replace all occurrences of the search string with the replacement string
 func StrReplace(s, old, new string, n int) string {
 
 	return strings.Replace(s, old, new, n)
 }
 
-//Strtolower - Make a string lowercase
+// Strtolower - Make a string lowercase
 func Strtolower(s string) string {
 
 	return strings.ToLower(s)
 }
 
-//Strtoupper - Make a string uppercase
+// Strtoupper - Make a string uppercase
 func Strtoupper(s string) string {
 
 	return strings.ToUpper(s)
 }
 
-//Strstr - Find the first occurrence of a string
+// Strstr - Find the first occurrence of a string
 func Strstr(s, substr string) int {
 
 	return strings.Index(s, substr)
 }
 
-//Strpos - Find the position of the first occurrence of a substring in a string
+// Strpos - Find the position of the first occurrence of a substring in a string
 func Strpos(s, substr string) int {
 
 	return strings.Index(s, substr)
 }
 
-//Stripos - Find the position of the first occurrence of a case-insensitive substring in a string
+// Stripos - Find the position of the first occurrence of a case-insensitive substring in a string
 func Stripos(s, substr string) int {
 
 	s = strings.ToLower(s)
@@ -214,13 +214,13 @@ func Stripos(s, substr string) int {
 	return strings.Index(s, substr)
 }
 
-//Strrpos - Find the position of the last occurrence of a substring in a string
+// Strrpos - Find the position of the last occurrence of a substring in a string
 func Strrpos(s, substr string) int {
 
 	return strings.LastIndex(s, substr)
 }
 
-//Strripos - Find the position of the last occurrence of a case-insensitive substring in a string
+// Strripos - Find the position of the last occurrence of a case-insensitive substring in a string
 func Strripos(s, substr string) int {
 
 	s = strings.ToLower(s)
@@ -229,7 +229,7 @@ func Strripos(s, substr string) int {
 	return strings.LastIndex(s, substr)
 }
 
-//Strrchr - Find the last occurrence of a character in a string
+// Strrchr - Find the last occurrence of a character in a string
 func Strrchr(s, substr string) string {
 
 	i := strings.LastIndex(s, substr)
@@ -240,20 +240,20 @@ func Strrchr(s, substr string) string {
 	return s[i:]
 }
 
-//Strlen - Get string length
+// Strlen - Get string length
 func Strlen(s string) int {
 
 	return len(s)
 }
 
-//MbStrlen - Get string length
+// MbStrlen - Get string length
 func MbStrlen(s string) int {
 
-	//return len([]rune(s))
+	// return len([]rune(s))
 	return utf8.RuneCountInString(s)
 }
 
-//Strrev - Reverse a string
+// Strrev - Reverse a string
 func Strrev(s string) string {
 
 	runes := []rune(s)
@@ -264,7 +264,7 @@ func Strrev(s string) string {
 	return string(runes)
 }
 
-//Substr - Return part of a string
+// Substr - Return part of a string
 func Substr(s string, start int, length ...int) string {
 
 	if len(length) > 0 {
@@ -287,7 +287,7 @@ func Substr(s string, start int, length ...int) string {
 	return s[start:]
 }
 
-//MbSubstr - Get part of string
+// MbSubstr - Get part of string
 func MbSubstr(s string, start int, length ...int) string {
 
 	runes := []rune(s)
@@ -311,13 +311,13 @@ func MbSubstr(s string, start int, length ...int) string {
 	return string(runes[start:])
 }
 
-//SubstrCount - Count the number of substring occurrences
+// SubstrCount - Count the number of substring occurrences
 func SubstrCount(s, substr string) int {
 
 	return strings.Count(s, substr)
 }
 
-//Ucfirst - Make a string's first character uppercase
+// Ucfirst - Make a string's first character uppercase
 func Ucfirst(s string) string {
 
 	runes := []rune(s)
