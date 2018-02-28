cd9532227364a135526583d59a4cbcf763776fb1
Openset <openset.wang@gmail.com>
2018-02-28 17:41:17 +0800

    Update: is file comment

--- php/is_file.go
+++ php/is_file.go
@@ -2,7 +2,7 @@ package php
 
 import "os"
 
-//IsFile Tells whether the filename is a regular file
+// IsFile Tells whether the filename is a regular file
 func IsFile(name string) bool {
 
 	var exist = true
