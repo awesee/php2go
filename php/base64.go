be0ad16c178dbc9fd46953cf7bb91a0e8d3c8302
Openset <openset.wang@gmail.com>
2018-02-28 17:40:01 +0800

    Update: base64 comment

--- php/base64.go
+++ php/base64.go
@@ -2,13 +2,13 @@ package php
 
 import "encoding/base64"
 
-//Base64Encode - Encodes data with MIME base64
+// Base64Encode - Encodes data with MIME base64
 func Base64Encode(s string) string {
 
 	return base64.StdEncoding.EncodeToString([]byte(s))
 }
 
-//Base64Decode - Decodes data encoded with MIME base64
+// Base64Decode - Decodes data encoded with MIME base64
 func Base64Decode(s string) (string, error) {
 
 	bt, err := base64.StdEncoding.DecodeString(s)
