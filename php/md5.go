4729f6f79d9974996f80840f4c0a49a96ae4c2cf
openset <openset.wang@gmail.com>
2019-07-22 13:08:43 +0800

    Update: md5

--- php/md5.go
+++ php/md5.go
@@ -8,20 +8,16 @@ import (
 
 // Md5 - Calculate the md5 hash of a string
 func Md5(s string) string {
-
-	byte := md5.Sum([]byte(s))
-
-	return hex.EncodeToString(byte[:])
+	hash := md5.Sum([]byte(s))
+	return hex.EncodeToString(hash[:])
 }
 
 // Md5File - Calculates the md5 hash of a given file
 func Md5File(filename string) string {
-
 	data, err := ioutil.ReadFile(filename)
 	if err != nil {
 		return ""
 	}
-	byte := md5.Sum(data)
-
-	return hex.EncodeToString(byte[:])
+	hash := md5.Sum(data)
+	return hex.EncodeToString(hash[:])
 }
