f8647ae8705bb6d7879d5033c3444eebbe41b37f
openset <openset.wang@gmail.com>
2019-09-04 13:19:56 +0800

    Add: sleep

--- php/sleep.go
+++ php/sleep.go
@@ -4,6 +4,10 @@ import "time"
 
 // Sleep - Delay execution
 func Sleep(s int64) {
-
 	time.Sleep(time.Duration(s) * time.Second)
+}
+
+// Usleep - Delay execution in microseconds
+func Usleep(ms int64) {
+	time.Sleep(time.Duration(ms) * time.Microsecond)
 }
