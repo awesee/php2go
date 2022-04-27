e65faa2efaf418496000fcc844ae014fdd46b0b5
Shuo <openset.wang@gmail.com>
2022-04-27 15:34:56 +0800

    A: RandomBytes

--- php/random_bytes.go
+++ php/random_bytes.go
@@ -2,6 +2,7 @@ package php
 
 import "crypto/rand"
 
+// RandomBytes — Generates cryptographically secure pseudo-random bytes
 func RandomBytes(len int) []byte {
 	b := make([]byte, len)
 	_, _ = rand.Read(b)
